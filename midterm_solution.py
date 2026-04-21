bdfea42605db8c064e287b1529c0076cce4349cd
Sandy Lei-Ann Roa Pascual <pascualsandyleiann@gmail.com>
2026-04-21 23:26:14 +0800

    Add student expense tracker functionality
    Initial implementation of a student expense tracker with categories and example expenses.

---
+++ midterm_solution.py
@@ -0,0 +1,20 @@
+print("\n=== Student Expense Tracker ===\n")
+
+student_name = input("Enter your name: ")
+weekly_budget = float(input("Enter your weekly budget: "))
+
+categories = [
+    "Food & Drinks",
+    "Transportation",
+    "Mobile / Internet",
+    "School Supplies",
+    "Entertainment"
+]
+
+example_expenses = [
+    "Lunch, snacks, coffee",
+    "Bus, jeepney, ride-share",
+    "Load, data plan, WiFi top-up",
+    "Notebook, pen, bond paper",
+    "Games, movies, hangout"
+]
