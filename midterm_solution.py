2bd89025898f875348eaef5c10ee7a0b60d997e3
Sandy Lei-Ann Roa Pascual <pascualsandyleiann@gmail.com>
2026-04-21 23:28:13 +0800

    Add expense logging and budget status features

--- midterm_solution.py
+++ midterm_solution.py
@@ -29,3 +29,31 @@ print("-" * 66)
 for i in range(len(categories)):
     print(f"| {i+1:<3} | {categories[i]:<20} | {example_expenses[i]:<34} |")
 print("-" * 66)
+
+for i in range(1, 5):
+    print(f"\n--- EXPENSE {i} ---")
+    category_number = int(input("Category (1-5 or 0 to skip): "))
+    
+    if category_number == 0:
+        print("Expense slot skipped.")
+        continue
+    
+    if 1 <= category_number <= 5:
+        description = input("Description: ")
+        amount = float(input("Amount: "))
+        
+        alert = ""
+        if amount > (weekly_budget * 0.25):
+            alert = "! High Expense Alert!"
+            
+        logged_expenses.append([i, categories[category_number-1], description, amount, alert])
+        total_spent += amount
+    else:
+        print("Invalid category. Slot skipped.")
+
+remaining_balance = weekly_budget - total_spent
+status = ""
+if remaining_balance >= 0:
+    status = "Budget OK! Keep it up."
+else:
+    status = "Overspent! Reduce spending."
